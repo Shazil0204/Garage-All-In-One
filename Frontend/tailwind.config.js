63bfa776d8d0c5209f89f81dfb7eaa950a4a4ed0
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-12-02 12:04:44 +0100

    installed Fluid Tailwind css

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+import fluid, { extract, screens, fontSize } from "fluid-tailwind";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    extract, // Keep extract here
+  ],
   theme: {
+    screens, // Fluid Tailwind’s default screens in rem
+    fontSize, // Fluid Tailwind’s default font sizes in rem
     extend: {
-      colors: {
-        primary: "#00A0A0",
-        secondary: "#008080"
+      screens: {
+        xs: "20rem", // Custom screen size (example)
       },
-    }
+      colors: {
+        primary: "#008080",
+        secondary: "#00A0A0",
+      },
+    },
   },
-  plugins: [],
+  plugins: [fluid],
 };
