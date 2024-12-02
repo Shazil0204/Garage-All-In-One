be55acee2980d4c6fbca65f0ec6f04d41ac97314
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-12-02 13:09:55 +0100

    dropdown style is working

--- Frontend/src/Components/Common/Dropdown.tsx
+++ Frontend/src/Components/Common/Dropdown.tsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+
 const Dropdown = () => {
-  return <div>Dropdown</div>;
+  const [title, setTitle] = useState("Category");
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+  const [isRotated, setIsRotated] = useState(false);
+
+  const ChangeDropDownValue = () => {
+    setDropDownOpen(!dropDownOpen);
+    setIsRotated(!isRotated); // Toggle rotation on button click
+  };
+
+  return (
+    <div className="relative max-w-min bg-primary text-white ~sm/lg~text-xs/md uppercase font-bold rounded-t-lg">
+      <button onClick={ChangeDropDownValue} className="flex gap-2 p-2">
+        {title}
+        <IoIosArrowDropdownCircle
+          id="icon"
+          className="text-xl mt-1 duration-1000"
+          style={{ transform: isRotated ? "rotate(540deg)" : "rotate(0deg)" }} // Apply rotation dynamically
+        />
+      </button>
+      {/* {dropDownOpen && (
+        <div className="absolute bg-primary p-2 min-w-full border-t-2 border-black duration-300 hover:bg-secondary rounded-b-lg"></div>
+      )} */}
+      <div
+        className={`absolute bg-primary p-2 min-w-full border-t-2 border-black hover:bg-secondary rounded-b-lg transition-all duration-500 ease-in-out ${
+          dropDownOpen ? "opacity-100 max-h-40" : "opacity-0 max-h-0"
+        }`}
+        style={{ overflow: "hidden" }}
+      >
+        {/* Dropdown content */}
+        <p>Option 1</p>
+        <p>Option 2</p>
+        <p>Option 3</p>
+        <p>Option 3</p>
+        <p>Option 3</p>
+        <p>Option 3</p>
+        <p>Option 3</p>
+        <p>Option 3</p>
+        <p>Option 3</p>
+        <p>Option 3</p>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
--- Frontend/src/Screens/Routes/Inventory.tsx
+++ Frontend/src/Screens/Routes/Inventory.tsx
@@ -1,8 +1,10 @@
+import Dropdown from "../../Components/Common/Dropdown";
+
 const Inventory = () => {
   return (
-    <>
-      <div>Inventory</div>
-    </>
+    <div className="p-12">
+      <Dropdown />
+    </div>
   );
 };
 
