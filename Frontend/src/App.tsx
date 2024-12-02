fb72cb969b0985603304c01d6746de84628b135e
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-12-02 08:41:03 +0100

    Data for routing is up and running

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,40 @@
+// React Route Dependencies
+import { Route, Routes } from "react-router-dom";
+
+// Layout
+import Sidebar from "./Components/Layout/Sidebar";
+
+// Screens
+import Inventory from "./Screens/Routes/Inventory";
+import Invoice from "./Screens/Routes/Invoice";
+import Issue from "./Screens/Routes/Issue";
+import Note from "./Screens/Routes/Note";
+import Car from "./Screens/Routes/Car";
+import Login from "./Screens/Login";
+import NotFound from "./Screens/NotFound";
+
+// This way i will be able to use it in both route and sidebar
+const sections = [
+  { id: "Login", element: <Login /> },
+  { id: "/", element: <Inventory /> },
+  { id: "Car", element: <Car /> },
+  { id: "Issue", element: <Issue /> },
+  { id: "Invoice", element: <Invoice /> },
+  { id: "Notes", element: <Note /> },
+  { id: "*", element: <NotFound /> },
+];
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Sidebar sections={sections} />
+      <Routes>
+        {sections.map((section) => (
+          <Route path={section.id} element={section.element} />
+        ))}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- Frontend/src/Components/Layout/Sidebar.tsx
+++ Frontend/src/Components/Layout/Sidebar.tsx
@@ -1,5 +1,17 @@
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import React from "react";
+
+interface SidebarProps {
+  sections: { id: string; element: JSX.Element }[];
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ sections }) => {
+  return (
+    <div>
+      {sections.map((section) => (
+        <div key={section.id}>{section.element}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Sidebar;
