2273e1470fcbf59f8ea9608a4e02462c0e7d0964
Jorjo6712 <kiro.nicolov@gmail.com>
2024-12-03 10:12:54 +0100

    sidebar done

--- Frontend/src/Components/Layout/NavLink.tsx
+++ Frontend/src/Components/Layout/NavLink.tsx
@@ -11,14 +11,14 @@ interface NavLinkProps {
 const NavLink: React.FC<NavLinkProps> = ({
   to,
   children,
-  className = "",
-  activeClassName = "text-blue-500 font-bold",
+  className = "~px-24/20",
+  activeClassName = "text-white bg-primary font-bold text-md hover:!bg-primary hover:!bg-opacity-100",
 }) => {
   return (
     <RouterNavLink
       to={to}
       className={({ isActive }) =>
-        `${className} ${isActive ? activeClassName : ""}`.trim()
+        `${className} ${isActive ? activeClassName : "font-bold text-md text-white"}`
       }
     >
       {children}
--- Frontend/src/Components/Layout/Sidebar.tsx
+++ Frontend/src/Components/Layout/Sidebar.tsx
@@ -1,28 +1,47 @@
+// React imports
 import React, { useState } from "react"
+
+// ReactIcons
+import { RxCross1 } from "react-icons/rx"
+
+// Layout imports
 import NavLink from "../Layout/NavLink"
 
 interface SidebarProps {
-  sections: { id: string, element: React.ReactNode}[]
+  sections: { id: string, element: React.ReactNode, icon: React.ReactNode }[]
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ sections }) => {
-  const [isExpanded] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(false)
+  
+  const handleMouseEnter = () => setIsExpanded(true)
+  const handleMouseLeave = () => setIsExpanded(false)
 
   return (
     <div
-      className={`absolute z-40 flex flex-col items-center w-16 h-full overflow-hidden text-gray-700 bg-[#00A0A0]
-        transition-all duration-300 ease-in-out group hover:w-64 hover:overflow-visible`}
+      className={`absolute z-40 flex flex-col justify-center items-center xl:w-16 w-2/12 h-full overflow-hidden bg-[#00A0A0]
+        transition-all duration-300 ease-in-out ${isExpanded ? "hover:~w-64/72" : "xl:w-16 md:w-16" }`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      <ul className="mt-4 space-y-2">
+      <button className={`${isExpanded ? "absolute" : "hidden" } top-0 left-0 mt-4 ml-4 py-4 px-4 bg-white rounded`} onClick={ () => setIsExpanded(false) }> <RxCross1 /> </button>
+
+      <ul className="mb-24 space-y-6">
         {sections.map((section) => (
-          <li key={section.id} className="relative group">
+          <li key={section.id} className="relative">
             <NavLink
               to={section.id}
-              className={`flex items-center px-4 py-2 z-50 bg-gray-200 rounded transition-colors duration-200
-                ${isExpanded ? "justify-start" : "justify-center"} `}
+              className={`flex items-center z-50 hover:bg-gray-300 hover:bg-opacity-20 hover:text-opacity-100 rounded transition-colors duration-200
+                ${isExpanded ? "justify-start py-4 px-16" : "justify-center py-4 px-4"}`}
             >
-              <span className="mr-4">{section.icon}</span>
-              { isExpanded ? '' : <span>{section.id} </span> }
+              {isExpanded ? (
+                <>
+                  <span className="mr-4">{section.icon}</span>
+                  <span>{section.id === "/" ? "Inventory" : section.id}</span>
+                </>
+              ) : (
+                <span>{section.icon}</span>
+              )}
             </NavLink>
           </li>
         ))}
