e677d04033f5350b72a5789608b7ed5dfd6ef5b9
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-12-03 10:32:42 +0100

    Update Modal.tsx

--- Frontend/src/Components/Common/Modal.tsx
+++ Frontend/src/Components/Common/Modal.tsx
@@ -1,5 +1,19 @@
-const Modal = () => {
-  return <div>Modal</div>;
+interface ModalProp {
+  children: React.ReactNode;
+  onClickOutside: () => void;
+}
+
+const Modal: React.FC<ModalProp> = ({ children, onClickOutside }) => {
+  return (
+    <div
+      onClick={onClickOutside}
+      className="w-full min-h-full bg-black/50 flex justify-center items-center fixed top-0 left-0 z-50 "
+    >
+      <div className="bg-white p-6 rounded-lg shadow-2xl max-w-md w-full border-2 border-black">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
