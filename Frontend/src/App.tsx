cd242fb89f75700eb0629dcbae3eb9100af0f2a5
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-12-03 12:50:25 +0100

    navbar is ready
    i will change the name of the sidebar component to navbar when we merge it again

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,46 +1,63 @@
 // React Route Dependencies
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
 // ReactIcons
-import { MdOutlineInventory } from "react-icons/md"
-import { FaCar } from "react-icons/fa"
-import { BsFillWrenchAdjustableCircleFill } from "react-icons/bs"
-import { FaFileInvoiceDollar } from "react-icons/fa"
-import { FaNoteSticky } from "react-icons/fa6"
+import { MdOutlineInventory } from "react-icons/md";
+import { FaCar } from "react-icons/fa";
+import { BsFillWrenchAdjustableCircleFill } from "react-icons/bs";
+import { FaFileInvoiceDollar } from "react-icons/fa";
+import { FaNoteSticky } from "react-icons/fa6";
 // Layout
-import Sidebar from "./Components/Layout/Sidebar"
+import Sidebar from "./Components/Layout/Sidebar";
 
 // Screens
-import Inventory from "./Screens/Routes/Inventory"
-import Invoice from "./Screens/Routes/Invoice"
-import Issue from "./Screens/Routes/Issue"
-import Note from "./Screens/Routes/Note"
-import Car from "./Screens/Routes/Car"
-import Login from "./Screens/Login"
-import NotFound from "./Screens/NotFound"
+import Inventory from "./Screens/Routes/Inventory";
+import Invoice from "./Screens/Routes/Invoice";
+import Issue from "./Screens/Routes/Issue";
+import Note from "./Screens/Routes/Note";
+import Car from "./Screens/Routes/Car";
+import Login from "./Screens/Login";
+import NotFound from "./Screens/NotFound";
+import Profile from "./Screens/Routes/Profile";
 
 // This way i will be able to use it in both route and sidebar
 const sections = [
-  { id: "/", element: <Inventory />, icon: <MdOutlineInventory/> },
-  { id: "Car", element: <Car />, icon: <FaCar/> },
-  { id: "Issue", element: <Issue />, icon: <BsFillWrenchAdjustableCircleFill/> },
-  { id: "Invoice", element: <Invoice />, icon: <FaFileInvoiceDollar/> },
-  { id: "Notes", element: <Note />, icon: <FaNoteSticky/>  },
-]
+  {
+    id: "/",
+    title: "Home",
+    element: <Inventory />,
+    icon: <MdOutlineInventory />,
+  },
+  { id: "/car", title: "Car", element: <Car />, icon: <FaCar /> },
+  {
+    id: "/issue",
+    title: "Issue",
+    element: <Issue />,
+    icon: <BsFillWrenchAdjustableCircleFill />,
+  },
+  {
+    id: "/invoice",
+    title: "Invoice",
+    element: <Invoice />,
+    icon: <FaFileInvoiceDollar />,
+  },
+  { id: "/notes", title: "Note", element: <Note />, icon: <FaNoteSticky /> },
+];
 
 const App = () => {
   return (
     <div>
-      {/* <Sidebar sections={sections} /> */}
+      <Sidebar sections={sections} />
       <Routes>
         <Route path="Login" element={<Login />} />
         {sections.map((section) => (
           <Route path={section.id} key={section.id} element={section.element} />
         ))}
+        <Route path="Profile" element={<Profile />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
