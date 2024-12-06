5496c31231ad4b532e7b64522eb7374883ed544e
Jorjo6712 <kiro.nicolov@gmail.com>
2024-12-06 23:14:10 +0100

    login working with complete jwt

--- Backend/app.js
+++ Backend/app.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const authRoutes = require('./src/routes/authRoutes');
 const sequelize = require('./src/config/database');
 const cors = require("cors");
 require('dotenv').config();
 
+// Controllers
+const registerController = require('./src/controllers/auth/register.controller');
+const loginController = require('./src/controllers/auth/login.controller');
+const authMiddleware = require('./src/middleware/auth.middleware');
+
+
 const app = express();
 
+// Middleware
 app.use(cors({
   origin: "http://localhost:5173",
   methods: ["GET", "POST"],
 }))
 
-// Middleware
 app.use(bodyParser.json());
 
 sequelize.sync({ force: false }) // force: false preserves existing tables
   .then(() => console.log('Database synced successfully'))
   .catch((err) => console.error('Error syncing database:', err));
 
-// Routes
-app.use('/api/auth', authRoutes);
-
-// Test protected route
-app.get('/api/protected', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the protected route!' });
-});
+console.log(registerController);
+// Auth Endpoints
+app.post('/api/auth/register', registerController);
+app.post('/api/auth/login', loginController);
 
 // Start server
 const PORT = process.env.PORT || 5000;
